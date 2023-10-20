780ed58ccdb5c0fbbffe8bd4f2778940d23c608d
hmtechnology <hmtechnology@users.noreply.github.com>
2023-10-20 02:39:45 +0200

    Update functions.php

--- woocommerce-store-hours-checker/functions.php
+++ woocommerce-store-hours-checker/functions.php
@@ -5,6 +5,11 @@ function reset_store_hours() {
     update_option('store_hours', array()); // Set store_hours to an empty array
 }
 
+// Function to completely closure dates
+function reset_closure_dates() {
+    update_option('closure_dates', array()); // Set closure_dates to an empty array
+}
+
 // Function to get if store is open or closed
 function get_store_hours_status() {
     return is_store_open() ? 'Open' : 'Closed';
@@ -25,11 +30,18 @@ function is_store_open() {
     $current_time = date('H:i');
 
     $store_hours = get_option('store_hours', array());
+    $closure_dates = get_option('closure_dates', array());
 
-	if (empty($store_hours)) {
-		return true; // The store is considered open if there are no configured hours.
-	}
-	
+    if (empty($store_hours)) {
+        return true; // The store is considered open if there are no configured hours.
+    }
+
+    // Check if the current date is a closure date
+    $current_date = date('Y-m-d');
+    if (in_array($current_date, $closure_dates)) {
+        return false; // Store is closed on the current date
+    }
+
     if (isset($store_hours[$current_day])) {
         foreach ($store_hours[$current_day] as $time_range) {
             if ($time_range['type'] === '24h') {
